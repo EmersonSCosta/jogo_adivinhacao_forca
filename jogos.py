93cffdc880dff2ad61d66d02a2884c47ca161a4c
EmersonSCosta <emerson@emersoncosta.com>
2021-01-28 02:04:02 -0300

    28/01/2021

---
+++ jogos.py
@@ -0,0 +1,17 @@
+import forca
+import adivinhacaoAtual
+
+print('●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●')
+print('●▬▬▬▬▬▬▬▬▬ஜ۩ Escolha um Game! ۩ஜ▬▬▬▬▬▬▬▬▬●')
+print('●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●')
+
+print('(1) Forca / (2) Adivinhação')
+
+jogo = int(input('Qual Jogo?'))
+
+if(jogo == 1):
+    print('Jogando Forca')
+elif(jogo == 2):
+    print('Jogando Adivinhação')
+else:
+    print('Jogo não Existe')
