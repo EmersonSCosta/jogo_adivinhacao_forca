ba93f93f1c2589b168a43c4bbadba88d5bbd03a4
EmersonSCosta <emerson@emersoncosta.com>
2021-01-28 02:44:06 -0300

    28/01/2021

--- jogos.py
+++ jogos.py
@@ -1,19 +1,26 @@
 import forca
 import adivinhacao
 
-print('●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●')
-print('●▬▬▬▬▬▬▬▬▬ஜ۩ Escolha um Game! ۩ஜ▬▬▬▬▬▬▬▬▬●')
-print('●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●')
 
-print('(1) Forca / (2) Adivinhação')
+def escolha_o_game():
 
-jogo = int(input('Qual Jogo? '))
+    print('●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●')
+    print('●▬▬▬▬▬▬▬▬▬ஜ۩ Escolha um Game! ۩ஜ▬▬▬▬▬▬▬▬▬●')
+    print('●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●')
 
-if(jogo == 1):
-    print('Jogando Forca')
-    forca.jogar()
-elif(jogo == 2):
-    print('Jogando Adivinhação')
-    adivinhacao.jogar()
-else:
-    print('Jogo não Existe')
+    print('(1) Forca / (2) Adivinhação')
+
+    jogo = int(input('Qual Jogo? '))
+
+    if(jogo == 1):
+        print('Jogando Forca')
+        forca.jogar()
+    elif(jogo == 2):
+        print('Jogando Adivinhação')
+        adivinhacao.jogar()
+    else:
+        print('Jogo não Existe')
+
+
+if(__name__ == '__main__'):
+    escolha_o_game()
